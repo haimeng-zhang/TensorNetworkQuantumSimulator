f8f2acbcd6d1f5185f42acbfcbe905a08c0e16f9
Joey <joeytindall94@googlemail.com>
2025-11-30 14:32:15 -0500

    Make an example test it

--- test/test_apply.jl
+++ test/test_apply.jl
@@ -8,7 +8,7 @@ using Test: @testset, @test
 @testset "Test Apply Circuit" begin
 
     #Custom circuit
-    circuit = [("Rx", [(1, 1)], 0.5), ("Rx", [(2, 1)], 0.2), ("CPHASE", [(1, 1), (2, 1)], -0.3)]
+    circuit = [("Rx", [(1, 1)], 0.5), ("Rx", (2, 1), 0.2), ("CPHASE", [(1, 1), (2, 1)], -0.3)]
     g = build_graph_from_circuit(circuit)
     ψ0 = tensornetworkstate(ComplexF32, v -> "↓", g)
     apply_kwargs = (; maxdim = 2, cutoff = 1.0e-10, normalize_tensors = false)
